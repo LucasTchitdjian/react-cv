8a09c59e2ac8f6d6452fc8d4e06a95ffb96b53ff
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-03-04 16:48:25 +0100

    nav-bar imported

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const Contact = () => {
   return (
     <div>
-      Hello depuis les contacts
+      <Navigation />
     </div>
   );
 };
