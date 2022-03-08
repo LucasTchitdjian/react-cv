84568c257cbdd27410f9a0aa6f28e642f6192e74
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-03-08 17:14:29 +0100

    homepage finished

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>
-      Not foound
+    <div className='notFound'>
+      <div className="notFoundContent">
+        <h3>Désolé cette page n'exsite pas</h3>
+        <NavLink exact to="/">
+          <i className='fas fa-home'></i>
+          <span>Accueil</span>
+        </NavLink>
+      </div>
     </div>
   );
 };
