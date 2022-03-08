f26dbf4e0870aabbb840103a0319798451121e6d
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-03-08 16:30:13 +0100

    end of styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,25 +13,25 @@ const Navigation = () => {
       <div className="navigation">
         <ul>
           <li>
-            <NavLink exact to="/" activeClassName="navActive">
+            <NavLink exact to="/">
               <i className='fas fa-home'></i>
               <span>Accueil</span> 
             </NavLink>
           </li>
           <li>
-            <NavLink exact to="/competences" activeClassName="navActive">
+            <NavLink exact to="/competences">
               <i className='fas fa-mountain'></i>
               <span>Competences</span> 
             </NavLink>
           </li>
           <li>
-            <NavLink exact to="/portfolio" activeClassName="navActive">
+            <NavLink exact to="/portfolio">
               <i className='fas fa-images'></i>
               <span>Portfolio</span> 
             </NavLink>
           </li>
           <li>
-            <NavLink exact to="/contact" activeClassName="navActive">
+            <NavLink exact to="/contact">
               <i className='fas fa-address-book'></i>
               <span>Contact</span> 
             </NavLink>
