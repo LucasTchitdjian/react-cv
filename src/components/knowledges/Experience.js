977ab5d2ca4e113819286e4db2007a0fffc9dfaa
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-03-09 19:16:12 +0100

    skills page part 1 finished

---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div className='experience'>
+      <h3>Expèriences</h3>
+      <div className="exp-1">
+        <h4>Directeur technique -Apple</h4>
+        <h5>2018-2020</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla amet, eligendi perferendis eos provident aliquid explicabo ad? Tenetur, tempore ducimus dolore magnam ad repellendus laboriosam expedita debitis pariatur cumque eaque dolorem laudantium quidem assumenda unde commodi odio voluptatum!</p>
+      </div>
+      <div className="exp-2">
+        <h4>Directeur technique -Apple</h4>
+        <h5>2018-2020</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla amet, eligendi perferendis eos provident aliquid explicabo ad? Tenetur, tempore ducimus dolore magnam ad repellendus laboriosam expedita debitis pariatur cumque eaque dolorem laudantium quidem assumenda unde commodi odio voluptatum!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
