1208df186108be09509e5cbded4122d89fa5795a
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-03-15 17:11:40 +0100

    portfolio page finished

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react'
 
 export default class Project extends Component {
 
+  state = {
+    showInfo: false
+  }
 
+  handleInfo = () => {
+    this.setState({
+      showInfo:!this.state.showInfo
+    })
+  }
 
   render() {
     let {name, languagesIcons, source, info, picture} = this.props.item;
@@ -16,10 +24,26 @@ export default class Project extends Component {
           )}
         </div>
         <h3>{name}</h3>
-        <img src={picture} alt="" />
-        <span className='infos'>
+        <img src={picture} alt="" onClick={this.handleInfo} />
+        <span className='infos' onClick={this.handleInfo}>
           <i className='fas fa-plus-circle'></i>
         </span>
+        {
+          this.state.showInfo && (
+            <div className="showInfos">
+              <div className="infosContent">
+                <div className="head">
+                  <h2>{name}</h2>
+                  <div className="sourceCode">
+                    <a href={source} rel="noopener noreferrer" className='button' target= "_blank">Code source</a>
+                  </div>
+                </div>
+                <div className="text">{info}</div>
+                <div className="button return" onClick={this.handleInfo}>Retourner sur la page</div>
+              </div>
+            </div>
+          )
+        }
       </div>
     )
   }
--- src/components/portfolio/ProjectList.js
+++ src/components/portfolio/ProjectList.js
@@ -4,13 +4,27 @@ import Project from './Project'
 
 export default class Projet extends Component {
 
+/* This is setting the state of the component to the portfolioData object. */
   state = {
-    projects:portfolioData
+    projects:portfolioData,
+    radios: [
+      {id: 1, value: "javascript"},
+      {id: 2, value: "css"},
+      {id: 3, value: "react"},
+      {id: 4, value: "php"},
+    ],
+    selectedRadio: 'javascript'
   };
+
+  handleRadio = (event) => {
+    let radio = event.target.value;
+    this.setState({selectedRadio: radio})
+  }
 
 
   render() {
-    let {projects} = this.state;
+  /* This is a JavaScript object that is being destructured. */
+    let {projects, radios, selectedRadio} = this.state;
 
 
 
@@ -18,11 +32,30 @@ export default class Projet extends Component {
     return (
       <div className="portfolioContent">
         <ul className='radioDisplay'>
-
+        {
+          radios.map((radio)=> {
+            return (
+              <li key={radio.id}>
+                <input 
+                 type="radio"
+                 name="radio"
+                 checked={radio.value === selectedRadio}
+                 value={radio.value}
+                 id={radio.value}
+                 onChange={this.handleRadio} 
+                />
+                <label htmlFor={radio.value}>{radio.value}</label>
+              </li>
+            )
+          })
+        }
         </ul>
         <div className="projects">
           {
-            projects.map(item => {
+            projects
+            .filter(item => item.languages.includes(
+              selectedRadio))
+            .map(item => {
               return (
                 <Project
                   key={item.id}  
